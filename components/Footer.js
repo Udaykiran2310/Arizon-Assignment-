e38839ee2e814776d129fdda1204e452c8ea46c6
Udaykiran2310 <103480671+Udaykiran2310@users.noreply.github.com>
2025-05-01 21:51:16 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-center p-4 bg-gray-800 text-white">
+      <p>© 2023 E-commerce App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
